9c273162050164e7827d4363f1ce82ef423cdacd
eageek <emmanuelassumani004@gmail.com>
2025-06-05 15:00:29 +0200

    valider

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,36 @@
 <template>
   <div class="App_cont">
-    <h1>Travail pratique de TODOLIST</h1>
     <div>
-      <form @submit.prevent="AddTache">
-        <input
-          type="text"
-          placeholder="Ajouter une tache"
-          v-model="AjoutTache"
-        />
-        <button>Ajouter</button>
-      </form>
+      <div>
+        <p v-hide="counter > 5">Test v-hide</p>
+      </div>
+      <div v-if="counter < 5" style="color: red">
+        <p>Vous avez clicker moins de 5 fois</p>
+      </div>
+      <div v-else style="color: green">
+        <p>Vous avez clicker plus de 5 fois</p>
+      </div>
     </div>
-    <div>
-      <ul>
-        <li v-for="Tache in ListTache" :key="Tache">
-          {{ Tache }}<button @click="DeleteTache(Tache)">Supprimer</button>
-          <input type="checkbox" />
-        </li>
-      </ul>
-    </div>
+    <h1>Je suis un compteur : {{ counter }}</h1>
+    <button @click="increment">Incrementer le compteur</button>
+    <button @click="decrementer">Decrementer</button>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const ListTache = ref(["Prendre le lait", "Prendre le the", "Prendre le cafe"]);
-const AjoutTache = ref("");
-const AddTache = () => {
-  if (AjoutTache.value === "") {
-    alert("Veuiller entre une tache valide");
-    return;
-  } else if (ListTache.value.includes(AjoutTache.value)) {
-    alert("Tache deja selectionner");
-    return;
-  }
-  AjoutTache.value = ListTache.value.unshift(AjoutTache.value);
-  AjoutTache.value = "";
+const counter = ref(0);
+
+const increment = () => {
+  counter.value++;
 };
-const DeleteTache = (Tache) => {
-  ListTache.value = ListTache.value.filter((m) => m != Tache);
+
+const decrementer = () => {
+  counter.value--;
 };
 </script>
 
 <style>
-* {
-  font-family: Arial, Helvetica, sans-serif;
-}
-h1 {
-  text-align: center;
-}
-form {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-form input {
-  width: 60%;
-  height: 40px;
-  margin: 5px;
-  border-radius: 5px;
-  border: 1px solid black;
-}
-form button {
-  width: 150px;
-  height: 40px;
-  border-radius: 5px;
-  border: 1px solid black;
-  cursor: pointer;
-}
-li {
-  list-style: none;
-  margin: 10px;
-  border: 1px solid black;
-  padding: 10px;
-}
-li button {
-  height: 30px;
-  width: 100px;
-  background-color: red;
-  border: none;
-  border-radius: 5px;
-  top: 0;
-  left: 300px;
-  position: relative;
-}
-li input {
-  top: 0;
-  left: 330px;
-  position: relative;
-}
+/*  */
 </style>
